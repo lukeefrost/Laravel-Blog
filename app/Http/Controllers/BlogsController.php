f830bf3bdaad46f2fd003d056c54e9745c9fe5cb
lukelliotfrost <luke@radio.co>
2019-05-09 18:10:14 +0100

    First commit with CRUD functionality for adding and removing blog entries.

---
+++ app/Http/Controllers/BlogsController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Blog;
+use Storage;
+
+class BlogsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $blogs = Blog::all();
+
+        return view('blogs.index', ['blogs' => $blogs]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('blogs.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+      $blog = new Blog;
+
+      $path = Storage::putFile('public', $request->file('images'));
+      $url =Storage::url($path);
+
+      $blog->image = $url;
+      $blog->title = $request->title;
+      $blog->content = $request->content;
+      $blog->save();
+
+      return redirect()->route('blogs_path');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $blog = Blog::find($id);
+
+        return view('blogs.show', ['blog' => $blog]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $blog = Blog::find($id);
+
+        return view('blogs.edit', ['blog' => $blog]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $blog = Blog::find($id);
+        $blog->title = $request->title;
+        $blog->content = $request->content;
+        $blog->update();
+
+        return redirect()->route('blog_path', ['blog' => $blog]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $blog = Blog::find($id);
+        $blog->delete();
+
+        return redirect()->route('blogs_path');
+    }
+}
---
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
+
+class VerifyCsrfToken extends BaseVerifier
+{
+    /**
+     * The URIs that should be excluded from CSRF verification.
+     *
+     * @var array
+     */
+    protected $except = [
+      '/blogs',
+      '/blogs/1',
+      '/blogs/2',
+      '/blogs/3',
+    ];
+}
